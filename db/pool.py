c17f6afdb01d5cb78fd7588803eed675982c3bcb
Prabhu Rajesh Pahanpate <prabhupahanpate@gmail.com>
2026-08-03 23:29:20 +0530

    Enhance connection pool initialization logic
    Refactor database connection pooling to support both URL and environment variable configurations.

--- db/pool.py
+++ db/pool.py
@@ -1,4 +1,5 @@
 from psycopg2.pool import SimpleConnectionPool
+from config.settings import DATABASE_URL, DB_HOST, DB_NAME, DB_USER, DB_PASS, DB_PORT
 import os
 import time
 
@@ -13,15 +14,29 @@ def init_pool():
 
     for _ in range(5):
         try:
-            pool = SimpleConnectionPool(
-                minconn=1,
-                maxconn=5,
-                host=os.getenv("DB_HOST"),
-                database=os.getenv("DB_NAME"),
-                user=os.getenv("DB_USER"),
-                password=os.getenv("DB_PASS"),
-                port=os.getenv("DB_PORT")
-            )
+            if DATABASE_URL:
+
+                print("[DB] Using Server PostgreSQL")
+
+                pool = SimpleConnectionPool(
+                    1,
+                    5,
+                    dsn=DATABASE_URL
+                )
+
+            else:
+
+                print("[DB] Using Local PostgreSQL")
+
+                pool = SimpleConnectionPool(
+                    1,
+                    5,
+                    host=DB_HOST,
+                    database=DB_NAME,
+                    user=DB_USER,
+                    password=DB_PASS,
+                    port=DB_PORT
+                )
 
             print("[DB] Pool created")
             return
